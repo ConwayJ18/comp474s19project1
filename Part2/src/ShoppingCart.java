86caedee9720325f94e25b296e4e6fae083c3e24
ConwayJ18 <conway.jess18@gmail.com>
2019-02-25 22:14:08 -0600

    Added ShoppingCart methods that comply to specifications

--- Part2/src/ShoppingCart.java
+++ Part2/src/ShoppingCart.java
@@ -1,4 +1,16 @@
+import java.util.List;
+
 public class ShoppingCart 
 {
+	ShoppingCart() {}
 	
+	String[] calculatePurchasePrice(List<String> ProductIDs, Customer customer)
+	{
+		return null;
+	}
+	
+	public static double roundOff(double price)
+	{
+		return 0;
+	}
 }
