08f775a6222de1f5730537f40ca6c41aaf745141
ConwayJ18 <conway.jess18@gmail.com>
2019-02-27 12:09:53 -0600

    Added method for Black Box Testing

--- Part2/src/ShoppingCartTest.java
+++ Part2/src/ShoppingCartTest.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 
-class ShoppingCartTest 
+class ShoppingCartTest
 {
 	@Test
 	void primaryCheckoutTests()
@@ -14,31 +14,31 @@ class ShoppingCartTest
 		//Initialize items to be placed in a Database, associating item ID to price
 		Database.addItem("1", 83.10);
 		Database.addItem("2");
-		
+
 		//Mock object to be used for testing
 		ShoppingCart sc = new ShoppingCart();
-		
+
 		//Not a Discount Member, not tax exempt
 		Customer c1 = new Customer(1);
-		
+
 		//Discount Member, not tax exempt
 		Customer c2 = new Customer(2, true);
-		
+
 		//Discount Member, tax exempt
 		Customer c3 = new Customer(3, true, true);
-		
+
 		//Not a Discount Member, tax exempt
 		Customer c4 = new Customer(4, false, true);
-		
+
 		//Quantity q < 5
 		ArrayList<String> smallCart = new ArrayList<String>(Arrays.asList("1", "2", "2"));
-		
+
 		//Quantity 5 <= q < 10
 		ArrayList<String> mediumCart = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2", "2"));
-		
+
 		//Quantity 10 <= q <= 50
 		ArrayList<String> largeCart = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2", "2", "2", "2", "2"));
-		
+
 		//Quantity q > 50
 		ArrayList<String> tooLargeCart = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																			"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
@@ -46,14 +46,14 @@ class ShoppingCartTest
 																			"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																			"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																			"2"));
-				
+
 		//Expected outputs, numbered according to previously submitted document
 		String[] eo1 = new String[] {"TooManyItemsError"};
 		String[] eo2 = new String[] {"$83.10", "$15.79", "$0.00", "$67.31"};
 		String[] eo3 = new String[] {"$83.10", "$15.79", "$3.03", "$70.34"};
 		String[] eo4 = new String[] {"$83.10", "$4.15", "$0.00", "$78.95"};
 		String[] eo5 = new String[] {"$83.10", "$0.00", "$3.74", "$86.84"};
-				
+
 		//Tests, numbered according to previously submitted document
 		assertArrayEquals(eo1, sc.calculatePurchasePrice(tooLargeCart, c3));
 		assertArrayEquals(eo2, sc.calculatePurchasePrice(largeCart, c3));
@@ -61,35 +61,35 @@ class ShoppingCartTest
 		//assertArrayEquals(eo4, sc.calculatePurchasePrice(mediumCart, c4));
 		assertArrayEquals(eo5, sc.calculatePurchasePrice(smallCart, c1));
 	}
-	
+
 	@Test
 	void baCheckoutTests()
 	{
 		//Initialize items to be placed in a Database, associating item ID to price
 		Database.addItem("1", 83.10);
 		Database.addItem("2");
-		
+
 		//Mock object to be used for testing
 		ShoppingCart sc = new ShoppingCart();
-		
+
 		//Not a Discount Member, tax exempt
 		Customer c4 = new Customer(4, false, true);
-		
+
 		//Quantity q = 4
 		ArrayList<String> four = new ArrayList<String>(Arrays.asList("1", "2", "2", "2"));
-		
+
 		//Quantity q = 5
 		ArrayList<String> five = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2"));
-				
+
 		//Quantity q = 6
 		ArrayList<String> six = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2"));
-		
+
 		//Quantity q = 9
 		ArrayList<String> nine = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2", "2", "2", "2"));
-		
+
 		//Quantity q = 10
 		ArrayList<String> ten = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2", "2", "2", "2", "2"));
-		
+
 		//Quantity q = 11
 		ArrayList<String> eleven = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																		"2"));
@@ -99,15 +99,15 @@ class ShoppingCartTest
 																		"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																		"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																		"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
-																		"2", "2", "2", "2", "2", "2", "2", "2", "2"));		
-		
+																		"2", "2", "2", "2", "2", "2", "2", "2", "2"));
+
 		//Quantity q = 50
 		ArrayList<String> fifty = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																		"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																		"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																		"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																		"2", "2", "2", "2", "2", "2", "2", "2", "2", "2"));
-				
+
 		//Quantity q = 51
 		ArrayList<String> fiftyOne = new ArrayList<String>(Arrays.asList("1", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																			"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
@@ -115,7 +115,7 @@ class ShoppingCartTest
 																			"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																			"2", "2", "2", "2", "2", "2", "2", "2", "2", "2",
 																			"2"));
-				
+
 		//Expected outputs, numbered according to previously submitted document
 		String[] eo6 = new String[] {"TooManyItemsError"};
 		String[] eo7 = new String[] {"$83.10", "$8.31", "$0.00", "$74.79"};
@@ -126,7 +126,7 @@ class ShoppingCartTest
 		String[] eo12 = new String[] {"$83.10", "$4.15", "$0.00", "$78.95"};
 		String[] eo13 = new String[] {"$83.10", "$4.15", "$0.00", "$78.95"};
 		String[] eo14 = new String[] {"$83.10", "$0.00", "$0.00", "$83.10"};
-		
+
 		//Tests, numbered according to previously submitted document
 		assertArrayEquals(eo6, sc.calculatePurchasePrice(fiftyOne, c4));
 		assertArrayEquals(eo7, sc.calculatePurchasePrice(fifty, c4));
@@ -138,7 +138,7 @@ class ShoppingCartTest
 		//assertArrayEquals(eo13, sc.calculatePurchasePrice(five, c4));
 		assertArrayEquals(eo14, sc.calculatePurchasePrice(four, c4));
 	}
-	
+
 	@Test
 	void roundOffTests()
 	{
@@ -149,5 +149,25 @@ class ShoppingCartTest
 		assertEquals("5.52", String.format("%.2f", ShoppingCart.roundOff(5.52)));
 		assertEquals("9.40", String.format("%.2f", ShoppingCart.roundOff(9.4)));
 		assertEquals("7.00", String.format("%.2f", ShoppingCart.roundOff(7)));
+	}
+
+	@Test
+	void blackBoxTests()
+	{
+		/*
+		 * Create objects used for black box tests here
+		 */
+		 ShoppingCart sc = new ShoppingCart();
+
+		 //Database.addItem(String itemID, double price)
+		 //Customer customer = new Customer(int customerID, boolean discountMember, boolean taxExempt);
+		 //ArrayList<String> itemList = new ArrayList<String>(Arrays.asList(String itemID1, String itemID2, ... , String itemIDn));
+
+		/*
+		 * Create tests themselves here
+		 */
+
+		//assertArrayEquals(String[] expectedOutputs, sc.calculatePurchasePrice(ArrayList<String> itemList, Customer customer));
+		//assertEquals(String expectedOutput, String.valueOf(ShoppingCart.roundOff(double testValue)));
 	}
 }
